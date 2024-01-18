ccb71f7b7e454763564f3ab9d041630ea565197d
Alan DAmico <adamico1@uncc.edu>
2024-01-18 16:40:18 -0500

    Implemented functionality for adding notes

--- src/games/practice/Cell.java
+++ src/games/practice/Cell.java
@@ -2,11 +2,17 @@ package games.practice;
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Set;
 
 public class Cell{
 
     // Swing
     private JPanel cell;
+
+    // Property Names
+    public static final String VISIBLE_NOTE = "visibleNote";
 
     // Game Fields
     private int playerValue;
@@ -14,22 +20,32 @@ public class Cell{
     private boolean isCorrect;
     private boolean[] writtenNotes = new boolean[9];
     private final boolean isLocked;
-    private Number number;
-    private Notes notes;
+    private final Number number;
+    private final Notes notes;
     private final String numberCard = "number";
     private final String notesCard = "notes";
+    private final PropertyChangeSupport PCS = new PropertyChangeSupport(this);
 
 
-    public Cell(int actualValue, boolean isLocked, Color backgroundColor, Color textColor) {
+    public Cell(int actualValue, boolean isLocked, Color backgroundColor, Color textColor, Font font) {
         this.actualValue = actualValue;
         this.isLocked = isLocked;
         this.playerValue = isLocked ? actualValue : 0;
+        this.isCorrect = isLocked;
 
-        number = isLocked ? new Number(actualValue) : new Number();
-        notes = new Notes(backgroundColor, textColor);
+        cell.setFocusable(true);
+
+        number = isLocked ? new Number(actualValue, backgroundColor, textColor, font) : new Number(backgroundColor, textColor, font);
+        notes = new Notes(backgroundColor, textColor, font, this);
 
         cell.add(number.getNumberPanel(), numberCard);
         cell.add(notes.getNotesPanel(), notesCard);
+    }
+
+    public void changeCard(){
+        CardLayout cl = (CardLayout) cell.getLayout();
+
+        cl.show(cell, playerValue == 0 ? notesCard : numberCard);
     }
 
     public int getPlayerValue() {
@@ -48,10 +64,6 @@ public class Cell{
         return isCorrect;
     }
 
-    public boolean[] getWrittenNotes() {
-        return writtenNotes;
-    }
-
     public boolean isLocked() {
         return isLocked;
     }
@@ -65,14 +77,57 @@ public class Cell{
         number.setValue(move);
 
         // Check if the numbers are equal and pass the T/F value into the Number Object to select a color to change to
-        number.setTextColorBasedOnValue(move == actualValue);
+        isCorrect = move == actualValue;
+        number.setTextColorBasedOnValue(isCorrect);
     }
 
     public void changeBackground(Color color){
         number.getUserInput().setBackground(color);
+
+        for(JTextField currentField: notes.getNotes()){
+            currentField.setBackground(color);
+        }
+
+        notes.changeBackgroundColor(color);
     }
 
     public JTextField getNumberTextField(){
         return number.getUserInput();
+    }
+
+    public Set<JTextField> getNoteTextFields(){
+        return notes.getNotes();
+    }
+
+    public boolean[] getWrittenNotes() {
+        return writtenNotes;
+    }
+
+    public void setWrittenNotes(boolean[] writtenNotes) {
+        boolean[] oldNotes = this.writtenNotes;
+        this.writtenNotes = writtenNotes;
+
+        PCS.firePropertyChange(VISIBLE_NOTE, oldNotes, writtenNotes);
+    }
+
+    public boolean getWrittenNote(int i) {
+        return writtenNotes[i];
+    }
+
+    public void setWrittenNote(int i) {
+        int index = i - 1;
+
+        boolean oldNote = writtenNotes[index];
+        writtenNotes[index] = !writtenNotes[index];
+
+        PCS.fireIndexedPropertyChange(VISIBLE_NOTE, index, oldNote, writtenNotes[index]);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        PCS.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        PCS.removePropertyChangeListener(listener);
     }
 }
